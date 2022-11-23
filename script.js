401f7339a6164d570ae91f333fb53120cacc2db7
김예리 <dpfl0445@gmail.com>
2022-11-24 00:27:03 +0900

    리뉴얼 시작!

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
-const mainImg0 = document.getElementsByClassName("backImg0")[0];
-const mainImg1 = document.getElementsByClassName("backImg1")[0];
-const mainImg2 = document.getElementsByClassName("backImg2")[0];
-const mainImg3 = document.getElementsByClassName("backImg3")[0];
+const mainImg = document.getElementsByClassName("mainBackImg");
+const mainText = document.getElementsByClassName("mainText_b");
+const nextBtn = document.getElementsByClassName("nextBtn")[0];
 let n = 0;
 
 setInterval(() => {
     if (n < 3) {
         if (n > 0) {
-            eval("mainImg" + (n - 1) + ".style.opacity = 0;");
+            mainImg[n - 1].style.opacity = 0;
         } else {
-            mainImg3.style.opacity = 0;
+            mainImg[3].style.opacity = 0;
         }
-        eval("mainImg" + n + ".style.opacity = 1;");
+        mainImg[n].style.opacity = 1;
         n++;
     } else {
-        eval("mainImg" + (n - 1) + ".style.opacity = 0;");
-        eval("mainImg" + n + ".style.opacity = 1;");
+        mainImg[n - 1].style.opacity = 0;
+        mainImg[n].style.opacity = 1;
         n = 0;
     }
 }, 3000);
+
+// window.addEventListener("mousemove", (e) => {
+//     for (let i = 0; i < 4; i++) {
+//         mainText[i].style.left = `${e.x / 5 + i * 5}px`;
+//     }
+// });
